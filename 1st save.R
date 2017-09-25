6eeb35056fd764931af6089f8f543ef4e3b7e307
Your Name <you@example.com>
2017-09-25 12:06:15 +0200

    2nd save

--- 1st save.R
+++ 1st save.R
@@ -80,3 +80,70 @@ f <- function(x) 2 * x + 1
 f(5)
 f(pi)
 
+## combine values into a vector
+
+c(1, 2, 3, 4, 5)
+f(x)
+x <- c(1, 2, 3, 4, 5)
+f(x)
+
+letters[c(1, 2, 3, 4, 5)]
+letters[1:5]
+
+1:5
+seq(1,5)
+?seq
+seq(1,5, 0.1)
+
+## now we store a sequence from 1 to 5 by 0.1 as x and plot f(x)
+x <- seq(1, 5, 0.1)
+plot(x, f(x))
+plot(x, f(x), type = 'l')
+
+## adding a title and a grid to the plot
+plot(x, f(x), type = 'l', xlab = '', main = '2x+1')
+grid()
+
+## TODO draw a sine wave
+x <- seq(0, pi * 2, 0.1)
+plot(x, sin(x), type = 'l')
+str(x)
+print(x)
+2*pi
+## a simpler way for plotting functions
+curve(f)
+?curve
+
+curve(2*x + 1, from = 0, to = 50)
+curve(x + 1, add = TRUE, col = 'red')
+print(x)
+
+## TODO draw a sine wave with curve
+curve(sin, from = 0, to = 2*pi, col = "red")
+curve(sin, from = x, to = 2*pi*x, col = "blue")
+
+## #############################################################################
+## simulate Brownian motion / random walk in 1D
+## #############################################################################
+
+## first steps
+round(runif(5))
+
+## we transform the previously generated floating random numbers between 0 and 1
+## to either -1 or 1
+round(runif(5))*2 - 1
+
+## we compute the cumulative sum to identify the actual value for each iteration
+cumsum(round(runif(25))*2 - 1)
+plot(cumsum(round(runif(25))*2 - 1), type = 's')
+?plot
+
+## set the random number generator to a specific state for reproducibility
+set.seed(42)
+plot(cumsum(round(runif(25))*2 - 1), type = 's')
+
+## a much simpler solution for the above
+plot(cumsum(sample(c(-1, 1), 25, replace = TRUE)), type = 's')
+?sample
+sample(5, 25, replace = TRUE)
+sample(c(-1, 1), 25, replace = TRUE)
